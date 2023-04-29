205841b8aa6ad72452c7ccab6eaefefa9a4a215c
Dineshkumaran <dineshguvara@gmail.com>
2023-04-29 10:17:22 +0530

    initial commit

--- src/pages/feedback/FeedbackForm.jsx
+++ src/pages/feedback/FeedbackForm.jsx
@@ -1,5 +1,12 @@
-import React, {useState, useEffect} from "react";
+import React, { useState, useEffect } from "react";
 import {
+  Table,
+  Thead,
+  Tbody,
+  Tr,
+  Th,
+  Td,
+  TableContainer,
   Box,
   Input,
   Stack,
@@ -9,16 +16,15 @@ import {
   FormErrorMessage,
   Flex,
   Textarea,
-  Heading
+  Heading,
 } from "@chakra-ui/react";
-import { useForm, Controller } from "react-hook-form";
+import { useForm, Controller, useFieldArray } from "react-hook-form";
 import { useNavigate, useParams, Link } from "react-router-dom";
-import {  ArrowBackIcon } from "@chakra-ui/icons";
+import { ArrowBackIcon } from "@chakra-ui/icons";
 import { Select } from "chakra-react-select";
 import { useSelector } from "react-redux";
- 
 
-const sorc = [
+const skill = [
   {
     value: "naukri",
     label: "Naukri",
@@ -30,58 +36,72 @@ const sorc = [
   {
     value: "indeed",
     label: "Indeed",
-  }
-]
-
+  },
+];
 
 function FeedbackForm() {
-
   let navi = useNavigate();
 
-  const { candidateName } = useSelector((state) => state.poste); 
+  const { candidateName } = useSelector((state) => state.poste);
 
-  const{register, control, handleSubmit, formState:{errors} }= useForm();
+  const {
+    register,
+    control,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
 
-  const formSubmiter =  (data) =>{   
+  const {
+    fields: itemFields,
+    append: appendItem,
+    remove: removeItem,
+  } = useFieldArray({
+    control,
+    name: "Skill",
+  });
+
+  if (itemFields.length === 0) {
+    appendItem();
+  }
+
+  const formSubmiter = (data) => {
     console.log(data);
-    navi('/');
-  }  
-  
+    navi("/");
+  };
 
   return (
     <>
-    <Box bg="white" p={5} mb={5}  style={{ borderRadius: "10px" }}>
-      <Flex alignItems='center' gap={2}  >
-          <Link to="/pro_upload">
-          <ArrowBackIcon w={6} h={6} /> 
-          </Link> &nbsp;
+      <Box bg="white" p={5} mb={5} style={{ borderRadius: "10px" }}>
+        <Flex alignItems="center" gap={2}>
+          <Link to="/feedback">
+            <ArrowBackIcon w={6} h={6} />
+          </Link>{" "}
+          &nbsp;
           <Heading as="h3" size="lg" color="gray.600">
             Feedback Form
           </Heading>
-          
-      </Flex>
-    </Box>
-    <form onSubmit={handleSubmit(formSubmiter)}>
-     < Box p={4} color="black" bg="white" style= {{ borderRadius: "10px" }}>
-       <Stack spacing={4}>
-      
-       <Controller
-            control={control}
-            name="candidate_name"
-            rules={{
+        </Flex>
+      </Box>
+      <form onSubmit={handleSubmit(formSubmiter)}>
+        <Box p={4} color="black" bg="white" style={{ borderRadius: "10px" }}>
+          <Stack spacing={4}>
+            <Controller
+              control={control}
+              name="candidate_name"
+              rules={{
                 required: "Please Select Candidate Name.",
-            }}
-            render={({
+              }}
+              render={({
                 field: { onChange, onBlur, value, name, ref },
                 fieldState: { error },
-            }) => (
+              }) => (
                 <FormControl isInvalid={!!error}>
-                <FormLabel color="gray.600"> Candidate Name </FormLabel>
-                <Select
+                  <FormLabel color="gray.600"> Candidate Name </FormLabel>
+                  <Select
                     name={name}
                     ref={ref}
                     onChange={(e) => {
-                    onChange(e);
+                      onChange(e);
                     }}
                     onBlur={onBlur}
                     value={value}
@@ -90,23 +110,72 @@ function FeedbackForm() {
                     getOptionValue={(e) => e.value}
                     placeholder="Select Candidate Name"
                     closeMenuOnSelect={true}
-                />
-                <FormErrorMessage>
-                    {error && error.message}
-                </FormErrorMessage>
+                  />
+                  <FormErrorMessage>{error && error.message}</FormErrorMessage>
                 </FormControl>
-            )}
-        />
+              )}
+            />
+            <TableContainer mt={8}>
+              <Table variant="simple">
+                <Thead>
+                  <Tr>
+                    <Th> Skills </Th>
+                    <Th> Candidate Skill</Th>
+                    <Th> Action </Th>
+                  </Tr>
+                </Thead>
+                <Tbody>
+                {
+                  itemFields &&
+                  itemFields.map((item, index) => {
+                    return (
+                      <Tr key={item.id}>                        
+                        <Td>{item.id} </Td>
+                        <Td>
+                        <FormControl isInvalid={errors.document}>
+                            
+                            <Input
+                              type="input"
+                              placeholder="enter something"
+                              {...register(`Skill.${index}.item`, {
+                                required: "Document is required",
+                              })}
+                              
+                            />
+                            <FormErrorMessage>
+                              {errors.document && errors.document.message}
+                            </FormErrorMessage>
+                          </FormControl>
+                        </Td>
+                        <Td>
+                          <Button colorScheme="red" size="sm" onClick={()=>removeItem(index)}>
+                            Delete
+                          </Button>
+                        </Td>
+                      </Tr>                       
+                    );
+                  })}
 
-       </Stack>
-       <Flex mt={10}>
-       <Button type="submit" colorScheme="blue">
-          Submit
-        </Button> &nbsp;
-       <Link to="/pro_upload"> <Button colorScheme="red" >Back</Button> </Link>
-       </Flex>
-      </Box>
-    </form> 
+                </Tbody>
+              </Table>
+            </TableContainer>              
+          </Stack>  
+            
+          <Button  colorScheme="blue" float="right" mt={5}  onClick={()=>appendItem()}> Add inputs </Button>
+           
+          <Flex mt={10}>
+            <Button type="submit" colorScheme="teal"> 
+              Submit
+            </Button>{" "}
+           
+            &nbsp;
+            <Link to="/pro_upload">
+              {" "}
+              <Button colorScheme="red">Back</Button>{" "}
+            </Link>
+          </Flex>
+        </Box>
+      </form>
     </>
   );
 }
